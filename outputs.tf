31e6e8dd3affe996799f462028ac13e9418355e7
Luis M. Gallardo D <lgallard@gmail.com>
2021-08-11 14:14:52 -0300

    Add stateless_group and stateful_group outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,13 @@ output "network_firewall_status" {
   description = "Nested list of information about the current status of the firewall."
   value       = var.enabled && var.create_network_firewall ? aws_networkfirewall_firewall.firewall[0].firewall_status : []
 }
+
+output "network_firewall_stateless_group" {
+  description = "Map of stateless group rules"
+  value       = var.enabled ? aws_networkfirewall_rule_group.stateless_rule_group : null
+}
+
+output "network_firewall_stateful_group" {
+  description = "Map of stateful group rules"
+  value       = var.enabled ? aws_networkfirewall_rule_group.stateful_rule_group : null
+}
