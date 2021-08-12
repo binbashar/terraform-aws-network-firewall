2c2c4d6dcef83db0eb8ed3144ec29f3ad49fa42c
Luis M. Gallardo D <lgallard@gmail.com>
2021-08-12 00:05:20 -0300

    Add test for Terratest

---
+++ Makefile
@@ -0,0 +1,25 @@
+.PHONY: help
+SHELL         := /bin/bash
+MAKEFILE_PATH := ./Makefile
+MAKEFILES_DIR := ./@bin/makefiles
+MAKEFILES_VER := v0.1.23
+
+help:
+	@echo 'Available Commands:'
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf " - \033[36m%-18s\033[0m %s\n", $$1, $$2}'
+
+#==============================================================#
+# INITIALIZATION                                               #
+#==============================================================#
+init-makefiles: ## initialize makefiles
+	rm -rf ${MAKEFILES_DIR}
+	mkdir -p ${MAKEFILES_DIR}
+	git clone https://github.com/binbashar/le-dev-makefiles.git ${MAKEFILES_DIR} -q
+	cd ${MAKEFILES_DIR} && git checkout ${MAKEFILES_VER} -q
+
+-include ${MAKEFILES_DIR}/circleci/circleci.mk
+-include ${MAKEFILES_DIR}/release-mgmt/release.mk
+-include ${MAKEFILES_DIR}/terraform14/terraform14-root-context.mk
+-include ${MAKEFILES_DIR}/terraform14/terraform14.mk
+-include ${MAKEFILES_DIR}/terratest14/terratest14.mk
+
---
+++ tests/network_firewall_test.go
@@ -0,0 +1,56 @@
+package test
+
+import (
+  "fmt"
+	"testing"
+  "strconv"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
+)
+
+// Check Network Firewall, stateless & stateful rules
+func TestTerraformNetworkFirewall(t *testing.T) {
+	t.Parallel()
+
+	expectedStatelessMap := map[string]string{"stateless-group-1": "100", }
+	expectedStatefulMap := map[string]string{"stateful-group-1": "Stateful Inspection for denying access to domains",  "stateful-group-2" : "Stateful Inspection for allowing access to domains", "stateful-group-3" : "Permits http traffic from source" }
+	minNumAzs := 1
+
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// website::tag::1::Set the path to the Terraform code that will be tested.
+		// The path to where our Terraform code is located
+		TerraformDir: "../examples/denying-access-to-domains",
+
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
+		NoColor: true,
+	})
+
+	// website::tag::4::Clean up resources with "terraform destroy". Using "defer" runs the command at the end of the test, whether the test succeeds or fails.
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// website::tag::2::Run "terraform init" and "terraform apply".
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables
+  actualStatelessMap := terraform.OutputMap(t, terraformOptions, "network_firewall_stateless_group")
+  actualStatefulMap := terraform.OutputMap(t, terraformOptions, "network_firewall_stateful_group")
+  //actualExample := terraform.Output(t, terraformOptions, "num_network_firewall_azs")
+  nfwAzs,err  := strconv.Atoi(terraform.Output(t, terraformOptions, "num_network_firewall_azs"))
+
+  fmt.Println(">>>>>> stateless:", actualStatelessMap)
+  fmt.Println(">>>>>> stateful:", actualStatefulMap)
+  fmt.Println(">>>>>> num of NFW AZs:", nfwAzs)
+
+  //intVar, err := strconv.Atoi(actualExample)
+  //fmt.Printf(">>>>>>intVar, = %T\n", intVar)
+  fmt.Printf(">>>>>>err, = %T\n", err)
+
+	// website::tag::3::Check the output against expected values.
+	// Verify we're getting back the outputs we expect
+  assert.Equal(t, expectedStatelessMap, actualStatelessMap)
+  assert.Equal(t, expectedStatefulMap, actualStatefulMap)
+  assert.GreaterOrEqual(t, nfwAzs, minNumAzs)
+}
